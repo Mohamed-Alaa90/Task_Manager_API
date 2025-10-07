06eb1b2842193cafed8bbf1c6d6b5244eaba83c6
Mohamed_Alaa <m7md3laa500@gmail.com>
2025-10-07 23:33:36 +0300

    implement user authentication and task management features

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Pest\Plugins\Only;
 
 class AuthController extends Controller
 {
@@ -34,6 +35,33 @@ class AuthController extends Controller
             ]
         ], 200);
     }
-    public function login() {}
-    public function logout() {}
+    public function login(Request $request)
+    {
+        $validated = $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string'
+        ]);
+
+        $user = User::where('email', $validated['email'])->first();
+
+        if (!$user || !Hash::check($validated['password'], $user->password)) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'wrong email or password'
+            ], 401);
+        }
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'User Logged In Successfully',
+            'access_token' => $token,
+            'data' => [
+                $user->only(['id', 'name', 'email'])
+            ]
+        ]);
+    }
+    public function logout(Request $request) {
+        
+    }
 }
