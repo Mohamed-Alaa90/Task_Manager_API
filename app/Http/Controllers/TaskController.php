a007a37014a6a8910cfc76663a52e146dac9c489
Mohamed_Alaa <m7md3laa500@gmail.com>
2025-10-08 14:44:36 +0300

    add show in Task controller

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Request;
+use  Illuminate\Http\JsonResponse as json;
 class TaskController extends Controller
 {
-    public function index(Request $request)
+    public function index(Request $request):json
     {
         $tasks = $request->user()->tasks;
         return response()->json([
@@ -17,7 +17,7 @@ class TaskController extends Controller
         ]);
     }
 
-    public function store(Request $request)
+    public function store(Request $request): json
     {
         $validated = $request->validate([
             'title' => 'required|string|max:255',
@@ -28,11 +28,28 @@ class TaskController extends Controller
         ]);
 
         $task = $request->user()->tasks()->create($validated);
-
         return response()->json([
             'status' => 'success',
             'data' => [
                 'task' => $task
+            ]
+        ]);
+    }
+
+    public function show(Request $request,$id): json
+    {
+        $task = $request->user()->tasks()->find($id);
+
+        if (!$task){
+            return response()->json([
+                'status'=>'error',
+                'message'=>'Task not found'
+            ]);
+        }
+        return response()->json([
+            'status'=>'success',
+            'data'=>[
+                'task'=>$task
             ]
         ]);
     }
