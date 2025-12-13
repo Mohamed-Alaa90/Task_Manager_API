72fe1c8896026ca136553821062ea8db104334d4
Mohamed_Alaa <m7md3laa500@gmail.com>
2025-12-13 05:32:06 +0200

    feat: enhance task management with update and delete functionalities; remove description field

--- src/controller/TaskController.js
+++ src/controller/TaskController.js
@@ -1,10 +1,11 @@
+const { default: mongoose } = require("mongoose");
 const Task = require("../models/Task.Model.js");
 
 const createTask = async (req, res) => {
   try {
-    const { title, description } = req.body;
+    const { title } = req.body;
 
-    if (!title || !description) {
+    if (!title) {
       return res.status(400).json({
         success: false,
         message: "All fields are required",
@@ -12,7 +13,6 @@ const createTask = async (req, res) => {
     }
     const task = await Task.create({
       title,
-      description,
       userId: req.userId,
     });
 
@@ -33,18 +33,11 @@ const createTask = async (req, res) => {
 const getAllTasks = async (req, res) => {
   try {
     const tasks = await Task.find({ userId: req.userId });
-    if (tasks.length === 0) {
-      res.status(404).json({
-        success: false,
-        message: "no Tasks Found",
-        data: [],
-      });
-    }
 
     res.status(200).json({
       success: true,
       message: "tasks fetched Successfully",
-      data: [tasks],
+      data: tasks,
     });
   } catch (error) {
     console.log(error.message);
@@ -53,4 +46,87 @@ const getAllTasks = async (req, res) => {
     });
   }
 };
-module.exports = { createTask, getAllTasks };
+
+const updateTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (Object.keys(req.body).length === 0) {
+      return res.status(400).json({
+        success: false,
+        message: "Body cannot be empty",
+      });
+    }
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid Task Id",
+      });
+    }
+    const { title, completed } = req.body;
+    const updateData = {};
+
+    if (title !== undefined) updateData.title = title;
+    if (completed !== undefined) updateData.completed = completed;
+
+    const task = await Task.findOneAndUpdate(
+      { _id: id, userId: req.userId },
+      updateData,
+      { new: true }
+    );
+    if (!task) {
+      return res.status(404).json({
+        success: false,
+        message: "Task Not Found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Task Updated Successfully",
+      data: {
+        task,
+      },
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({
+      message: "Server Error",
+    });
+  }
+};
+
+const deleteTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid Task Id",
+      });
+    }
+    const task = await Task.findByIdAndDelete({
+      _id: id,
+      userId: req.userId,
+    });
+
+    if (!task) {
+      return res.status(404).json({
+        success: false,
+        message: "Task Not Found",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Task Deleted Successfully",
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({
+      success: false,
+      message: "Server Error",
+    });
+  }
+};
+module.exports = { createTask, getAllTasks, updateTask, deleteTask };
--- src/router/Task.Route.js
+++ src/router/Task.Route.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { createTask, getAllTasks } = require("../controller/TaskController.js");
+const {
+  createTask,
+  getAllTasks,
+  updateTask,
+  deleteTask,
+} = require("../controller/TaskController.js");
 const authMiddleware = require("../middlewares/auth.middleware.js");
 
 router.post("/createTask", authMiddleware, createTask);
 router.get("/getAllTasks", authMiddleware, getAllTasks);
+router.put("/updatedTask/:id", authMiddleware, updateTask);
+router.delete("/deleteTask/:id", authMiddleware, deleteTask);
 
 module.exports = router;
